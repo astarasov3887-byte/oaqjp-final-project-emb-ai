b174dc1835c4d6ffd0617b1444780c08f6fff17b
astarasov3887-byte <astarasov3887@gmail.com>
2026-09-13 16:00:24 +0200

    Fix implicit global variable and add encodeURIComponent + error handling

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,17 @@
-let RunSentimentAnalysis = ()=>{
-    textToAnalyze = document.getElementById("textToAnalyze").value;
+let RunSentimentAnalysis = () => {
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.getElementById("system_response").innerHTML = xhttp.responseText;
+            } else {
+                document.getElementById("system_response").innerHTML =
+                    "Something went wrong. Please try again.";
+            }
         }
     };
-    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
+    xhttp.open("GET", "emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze), true);
     xhttp.send();
 }
